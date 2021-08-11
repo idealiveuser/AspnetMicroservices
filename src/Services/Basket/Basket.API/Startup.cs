8790e9b8c573be5aff5f0c2099703e58c9fca968
CarlesLabrana <carles.labrana@idealiveconsulting.com>
2021-08-11 16:47:41 +0200

    BASKET.API: CRUD

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,15 +1,10 @@
+using Basket.API.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Basket.API
 {
@@ -26,12 +21,31 @@ namespace Basket.API
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
+            });
+
+            // General Configuration
+            services.AddScoped<IBasketRepository, BasketRepository>();
+
+
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
             });
+
+
+
+
+
         }
+
+
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
